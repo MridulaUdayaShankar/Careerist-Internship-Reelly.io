c4756cddf66d1ec7faaef4295e4bbe1c510a927c
Mridula Shankar <mridulas_macair@Mridulas-MacBook-Air.local>
2025-01-17 10:56:10 -0800

    Make some minor changes

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,4 +4,4 @@ from pages.base_page import BasePage
 class MainPage(BasePage):
 
     def open_main_page(self):
-        self.open('https://soft.reelly.io/sign-in')
+        self.open('https://soft.reelly.io/')
